2e4d52c30744f1034e5d23667a520b1ad0899bd5
Dayane_fariasA <dayanefarias236@gmail.com>
2024-06-09 21:29:51 +0000

    adicionadas as funções de buscar as perguntas e montar as perguntas no html

--- pages/resultado/resultado.js
+++ pages/resultado/resultado.js
@@ -1,4 +1,4 @@
-import { verificarTema, trocarTema } from "../../helpers/tema-helper.js"
+import {verificarTema, trocarTema} from "../../helpers/tema-helper.js"
 
 const botaoTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
